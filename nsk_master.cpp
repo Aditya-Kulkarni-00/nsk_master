5e575c5c4f187b6ec4449f0556762ff1a21765b8
aditya kulkarni <adityaajaykulkarni2000@gmail.com>
2024-08-04 15:14:32 +0530

    library structure

---
+++ nsk_master.cpp
@@ -0,0 +1,9 @@
+#include "nsk_master.h"
+
+ModbusChannel::ModbusChannel(){
+    // Constructor Implementation
+}
+
+void ModbusChannel::begin(){
+    // Initialize all the ports
+}
---
+++ nsk_master.h
@@ -0,0 +1,12 @@
+#ifndef NSK_MASTER_H
+#define NSK_MASTER_H
+
+#include <Arduino.h>
+
+class ModbusChannel{
+    public:
+        ModbusChannel();
+        void begin();
+}
+
+#endif
